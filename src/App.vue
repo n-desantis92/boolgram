0779c1bfd1814436a94b5f912fc5f44d0f2692e3
dsn <dsnwebdev@gmail.com>
2021-09-02 15:23:22 +0200

    create project & add nav
    create cartelle progetto aggiunti gli asset ,creato componente search e aggiunto la nav

---
+++ src/App.vue
@@ -0,0 +1,99 @@
+<template>
+  <div id="app">
+    <div id="nav" class="navigatore">
+      <div class="container-nav">
+        <div class="logo">
+          <img src="@/assets/logo.png" alt="logo instagram">
+        </div>
+        <Search></Search>
+        <nav>
+          <ul>
+            <li><i class="fas fa-home"></i></li>
+            <li><i class="far fa-paper-plane"></i></li>
+            <li><i class="far fa-compass"></i></li>
+            <li><i class="far fa-heart"></i></li>
+            <li><img src="@/assets/profile.jpg" alt="foto profilo"></li>
+          </ul>
+        </nav>
+      </div>
+    </div>
+    <main>
+
+    </main>
+  </div>
+</template>
+
+<script>
+import Search from "@/components/Search.vue"
+
+export default {
+  name: 'app',
+  data() {
+    return {
+    }
+  },
+  components : {
+    Search
+  }
+}
+</script>
+
+<style lang="scss">
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+  .navigatore {
+    width: 100%;
+    border-bottom: 1px solid rgba(var(--b6a,219,219,219),1);
+    display: flex;
+    justify-content: center;
+
+    .container-nav {
+      width: 60%;
+      height: 54px;
+      display: flex;
+      justify-content: space-between;
+      align-items: center;
+
+
+      .logo {
+        margin-left: 10px;
+        height: 65%;
+        width: 120px;
+        img {
+          height: 100%;
+        }
+      }
+
+      nav {
+        margin-right: 10px;
+        ul {
+          list-style: none;
+          display: flex;
+          align-items: center;
+          li {
+            padding: 3px;
+            margin: 0 5px;
+            i {
+              font-size: 22px;
+              cursor: pointer;
+            }
+            img {
+              width: 25px;
+              border-radius: 50%;
+            }
+          }
+        }
+      }
+    }
+  }
+
+  main {
+    background: rgba(var(--b3f,250,250,250),1);
+    height: calc(100vh - 55px);
+  }
+</style>
+
+
---
+++ src/components/Search.vue
@@ -0,0 +1,71 @@
+<template>
+    <div  id="search" class="cont-search">
+        <i class="fas fa-search"></i>
+        Cerca
+        <div class="cont-input" id="input-serch">
+          <input type="text" class="search" placeholder=" Cerca">
+          <i  class="fas fa-times-circle" id="close"></i>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "search",
+    data() {
+        
+        return {
+        }
+    },
+    props : {
+
+
+    }
+};
+</script>
+
+<style lang="scss">
+    .cont-search {
+      position: relative;
+      color: #a9a6a6;
+      width: 200px;
+      height: 50%;
+      background: rgba(var(--b3f,250,250,250),1);
+      border: solid 1px rgba(var(--b6a,219,219,219),1);
+      border-radius: 2px;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+
+      i {
+        font-size: 12px;
+      }
+      .cont-input {
+        visibility: inherit;
+        position: absolute;
+        top: 0;
+        right: 0;
+        bottom: 0;
+        left: 0;
+        display: flex;
+        align-items: center;
+
+        input {
+          height: 100%;
+          width: 180px;
+          border: none;
+          background: rgba(var(--b3f,250,250,250),1);
+          font-size: 16px;
+
+          &:focus {
+            outline:none;
+          }
+        }
+        i {
+          cursor: pointer;
+
+        }
+      }
+
+    }
+</style>
